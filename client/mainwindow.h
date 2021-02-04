d0e142d4790266cf28cb394027d269f390c89b8e
Gabriel Cuá <gcua@unis.edu.gt>
2021-02-03 18:06:36 -0600

    Master Original

--- client/mainwindow.h
+++ client/mainwindow.h
@@ -81,6 +81,9 @@ class MainWindow: public QMainWindow, public Quotient::UriResolverBase {
         void selectRoom(Quotient::Room* r);
         void logout(Connection* c);
 
+    protected:
+        void resizeEvent(QResizeEvent *newSize) override;
+
     private slots:
         void invokeLogin();
 
@@ -161,15 +164,3 @@ class MainWindow: public QMainWindow, public Quotient::UriResolverBase {
 
         void closeEvent(QCloseEvent* event) override;
 };
-© 2021 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
